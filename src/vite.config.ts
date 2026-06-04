01a97a3983d9a6de0ecb244cc74c7a6c8af902f1
Meowzz1 <150365001+QlapMeowz1@users.noreply.github.com>
2026-06-05 01:46:35 +0700

    Create vite.config.ts

---
+++ src/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  publicDir: false,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/react'),
+    },
+  },
+  build: {
+    manifest: 'manifest.json',
+    outDir: 'public/build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        'player-portal': path.resolve(__dirname, 'resources/react/main.tsx'),
+      },
+    },
+  },
+});
